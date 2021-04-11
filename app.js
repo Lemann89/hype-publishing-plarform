339179a384e9d27cc20460542b52097fe0eecd77
korotkov <vl.korotkov324@gmail.com>
2021-04-11 22:24:18 +0300

    cors

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(express.json({extended: true}));
 
 app.use('/api/auth', require('./controllers/auth.controller'));
