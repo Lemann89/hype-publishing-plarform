2831118729dae34c360ed4cbc24b5781a8ceb651
korotkov <vl.korotkov324@gmail.com>
2021-04-18 22:17:47 +0300

    feat: change env

--- client/environment.ts
+++ client/environment.ts
@@ -1,4 +1,4 @@
 export enum Environment {
-    //BASE_URL = 'https://hype-backend-123.herokuapp.com/api'
-    BASE_URL = 'http://localhost:2002/api'
+    BASE_URL = 'https://hype-backend-123.herokuapp.com/api'
+    //BASE_URL = 'http://localhost:2002/api'
 }
